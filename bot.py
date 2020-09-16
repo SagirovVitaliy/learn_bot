aaf8a059cdc3a219b9d7f526620fb2c09c4c0a7c
Vitaliy Sagirov <bitalko15@gmail.com>
2020-09-17 02:52:47 +0300

    трек бот

--- bot.py
+++ bot.py
@@ -1,9 +1,13 @@
 import logging
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+from random import randint, choice
+from glob import glob
+from emoji import emojize
 
 import settings
 import ephem
 import datetime
+
 
 logging.basicConfig(filename='bot.log', level=logging.INFO)
 
@@ -12,16 +16,62 @@ PROXY = {'proxy_url': settings.PROXY_URL, 'urllib3_proxy_kwargs': {'username': s
 
 
 def greet_user(update, context):
-    print('Вызван /start')
-    update.message.reply_text('Привет, пользователь! Ты вызвал команду /start')
+    context.user_data['emoji'] = get_smile(context.user_data)
+    update.message.reply_text(f"Здравствуй, пользователь {context.user_data['emoji']}!")
+
+
+def conversation(update, context):
+    context.user_data['emoji'] = get_smile(context.user_data)
+    update.message.text
+    update.message.reply_text(f"Он ещё не умеет разговаривать дурила {context.user_data['emoji']}!)")
+
+
+def get_smile(user_data):
+    if 'emoji' not in user_data:
+        smile = choice(settings.USER_EMOJI)
+        return emojize(smile, use_aliases=True)
+    return user_data['emoji']
+
+
+def play_random_numbers(user_number):
+    bot_number = randint(user_number-10, user_number+10)
+    if user_number > bot_number:
+        message = f'Ты загадал {user_number}, я загадал {bot_number}, ты выиграл!'
+    elif user_number == bot_number:
+        message = f'Ты загадал {user_number}, я загадал {bot_number}, ничья!'
+    else:
+        message = f'Ты загадал {user_number}, я загадал {bot_number}, я выиграл!'
+    return message
+
+
+def guess_numder(update, context):
+    print(context.args)
+    if context.args:
+        try:
+            user_number = int(context.args[0])
+            message = play_random_numbers(user_number)
+        except (TypeError, ValueError):
+            message = 'Введите целое число'
+    else:
+        message = 'Введите число'
+    update.message.reply_text(message)
+
+
+def send_cat_picture(update, context):
+    cat_photos_list = glob('images/cat*.jp*g')
+    cat_pic_filename = choice(cat_photos_list)
+    chat_id = update.effective_chat.id
+    context.bot.send_photo(chat_id=chat_id, photo=open(cat_pic_filename, 'rb'))
 
 
 def planet(update, context):
     date = datetime.date.today()
 
-    planeta = update.message.text.split()
+    print(context.args)
 
-    need_planet = planeta[1].capitalize()
+    planeta = context.args
+
+    need_planet = planeta[0].capitalize()
 
     print(need_planet)
 
@@ -40,7 +90,6 @@ def planet(update, context):
 
 
 
-
 def main():
     mybot = Updater(settings.API_KEY, use_context=True, request_kwargs=PROXY)
 
@@ -48,7 +97,10 @@ def main():
 
     dp.add_handler(CommandHandler('start', greet_user))
     dp.add_handler(CommandHandler('planet', planet))
-    dp.add_handler(MessageHandler(Filters.text, planet))
+    dp.add_handler(CommandHandler('guess', guess_numder))
+    dp.add_handler(CommandHandler('cat', send_cat_picture))
+
+    dp.add_handler(MessageHandler(Filters.text, conversation))
     
     logging.info("Бот стартовал")
 
